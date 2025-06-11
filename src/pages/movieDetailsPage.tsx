c964c90ac0dc8133851ed6adb363f4218757519a
fxwalsh <fxwalsh@wit.ie>
2025-06-11 13:34:15 +0100

    Refactor: Template list page; HTTP API calls centralised.

--- src/pages/movieDetailsPage.tsx
+++ src/pages/movieDetailsPage.tsx
@@ -6,6 +6,9 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { useParams } from "react-router-dom";
 import { MovieDetailsProps, MovieImage} from "../types/interfaces";// replace existing MoviePageProps import
+import { getMovie, getMovieImages } from "../api/tmdb-api";
+
+
 
 
 const styles = {
@@ -27,29 +30,18 @@ const MoviePage: React.FC= () => {
   const [images, setImages] = useState<MovieImage[]>([]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((movie) => {
-        // console.log(movie)
-        setMovie(movie);
-      });
+    getMovie(id ?? "").then((movie) => {
+      setMovie(movie);
+    });
   }, [id]);
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters)
-      .then((images) => {
-        setImages(images);
-      });
+    getMovieImages(id ?? "").then((images) => {
+      setImages(images);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
 
   return (
     <>
